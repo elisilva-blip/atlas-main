6eb6e5b0b332f18d18fa8fe4ab9e69f745802b48
elisilva-blip <nathielesilva153@gmail.com>
2026-05-31 20:00:21 -0300

    edicao.h

--- include/edicao.h
+++ include/edicao.h
@@ -1,8 +1,11 @@
+// Cabeçalho do módulo de edição — declara a função de edição de pessoas cadastradas
+
 #ifndef EDITAR_H
 #define EDITAR_H
 
 #include "pessoa.h"
 
+// Permite editar os campos de uma pessoa existente, identificada pelo ID
 void editar_pessoa(Pessoa pessoas[], int total);
 
 #endif
